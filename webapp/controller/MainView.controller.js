81960b4ab7f969f73299d00b2c818510a625958c
jdematiasa <jdematiasa@gmail.com>
2025-09-25 01:02:33 +0000

    modified:   webapp/controller/MainView.controller.js modified:   webapp/i18n/i18n.properties new file:   webapp/model/SelectionScreenMenu.json modified:   webapp/view/MainView.view.xml

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,10 +1,21 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
-    "use strict";
+  "sap/ui/core/mvc/Controller"
+],
+/**
+ * @param {typeof sap.ui.core.mvc.Controller} Controller
+ */
+function (Controller) {
+  "use strict";
 
-    return Controller.extend("mattns.invoices.controller.MainView", {
-        onInit() {
-        }
-    });
+  return Controller.extend("mattns.invoices.controller.MainView", {
+
+    onInit: function () {
+      const oJSONModel = new sap.ui.model.json.JSONModel();
+      const oView = this.getView();
+
+      oJSONModel.loadData("/model/SelectionScreenMenu.json");
+      oView.setModel(oJSONModel, "selectionScreen");
+    }
+
+  });
 });
